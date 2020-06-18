b58efa2b6e96f0b6b9901667782de6ba44e1fecc
Tyler Hancock <darklime@live.ca>
2020-06-18 05:23:36 -0600

    Initial plugin code.

---
+++ index.js
@@ -0,0 +1,44 @@
+export default function attacher(options) {
+
+    const Parser = this.Parser.prototype;
+
+    const nodeType = options.nodeType ?? 'underline';
+    const marker = options.marker ?? '__';
+    const classNames = options.classNames ?? ["underline"];
+    const tagType = options.tagType ?? 'ins';
+
+    Parser.inlineTokenizers.underline = function underlineTokenizer(eat, value, silent) {
+
+        if (value.startsWith(marker)) {
+
+            const end = value.indexOf(marker, 2);
+
+            if (end > -1) {
+
+                if (silent) {
+
+                    return true;
+                }
+
+                const text = value.substring(2, end);
+
+                const now = eat.now();
+                now.column += marker.length;
+                now.offset += marker.length;
+
+                return eat(marker + text + marker)({
+                    type: nodeType,
+                    children: this.tokenizeInline(text, now),
+                    data: {
+                        hName: tagType,
+                        hProperties: classNames.length ? { className: classNames } : {}
+                    }
+                });
+            }
+        }
+
+        return false;
+    };
+    Parser.inlineTokenizers.underline.locator = (value, fromIndex) => value.indexOf(marker, fromIndex);
+    Parser.inlineMethods.splice(Parser.inlineMethods.indexOf('strong'), 0, 'underline');
+};
